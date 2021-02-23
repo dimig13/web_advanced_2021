1b84ed95f99793938621a48ad0eae9d8562ef64a
Dimi Giannakos <12002222@student.pxl.be>
2021-02-23 14:17:24 +0100

    java script done deel 1

---
+++ Java script/oef js deel 1.js
@@ -0,0 +1,14 @@
+/* oef 2 */
+alert('Welkom');
+
+
+/* oef 3 */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+let cijfer = prompt('Vul hier uw cijfer in ', 'Uw cijfer');
+
+console.log(getRandomInt(5));
+
+
