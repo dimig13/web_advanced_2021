3c227111c5147e5b3be04e929f085a02a558511f
Dimi Giannakos <12002222@student.pxl.be>
2021-03-10 08:55:54 +0100

    java deel 3

--- Java script/deel 3/funct.js
+++ Java script/deel 3/funct.js
@@ -1,35 +1,43 @@
 /* oef 1 */
-function multiply(getal1 , getal2){
-return getal1 * getal2;
-}
 
-let uitkomst = multiply(5 , 7);
-console.log(uitkomst);
+// let dag = prompt('wat is je geboortejaar?', 'jaar');
+// var d = new Date(dag);
 
-/*
+// document.getElementById('demo').innerHTML = ;
 
 
-function difference(getal){
-    return '100' - getal;
-}
+/* oef 2*/
 
+// let sporten = prompt('sport je graag','ja/nee');
+// let uitkomst;
+// if (sporten === 'ja'){
+// uitkomst = "Just Do it"
+// }
+// else{
+// uitkomst = "Jammer,niet opgeven!"
+// }
+// document.getElementById('demo').innerHTML = uitkomst;
 
 /* oef 3 */
 
-function isfifthy(getal1 , getal2){
-    if (getal1 === 50 && getal2 ===50 ){
-        return bool= true;
-
-    }
-    else {
-
-    }
-}
-
+// let welkomszin = "Welkom op de webpagina!";
+// document.getElementById('demo').innerHTML = welkomszin;
+//
+// var d = new Date(); // dit geeft al huidige dag en tijd aan
+// document.getElementById('time').innerHTML = d;
+//
+// if (d.getHours() < 12 ){
+//     text = "voormiddag"
+// }
+// else{
+//     text = "Namiddag"
+// }
+//
+// document.getElementById('voorm').innerHTML = text;
 
 
 /* oef 4 */
 
-function startswithhello(string){
 
-}
+
+
